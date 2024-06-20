7d718d52e2155291b592a44b12d67b8a8accdcdf
Jarol  Moreno Jimenez <90114389+jarolmoreno@users.noreply.github.com>
2024-06-19 21:11:03 -0500

    insercion controlador ciudad

---
+++ BACKEND/Controlador/ciudad.php
@@ -0,0 +1,44 @@
+<?php
+
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type. Accept");
+
+    require_once ("../conexion.php");
+    require_once ("../Modelo/ciudad.php");
+
+    $control = $_GET['control'];
+
+    $prod = new Compras ( $conexion );
+
+    switch ($control) {
+
+        case 'consulta':
+          $vec= $prod -> consulta() ;
+        break;
+        case 'insertar':
+             $json = file_get_contents("php://input");
+             $params = json_decode($json);
+             $vec = $prod -> insertar($params);
+        break;
+        case 'eliminar':
+            $id = $_GET('id');
+            $vec = $prod -> eliminar ($id);
+
+        break;
+
+        case 'editar':
+            $json = file_get_contents("php://input");
+            $params = json_decode($json);
+            $id = $_GET('id');
+            $vec = $prod -> editar ($id, $params);
+
+            break;
+
+    }
+
+    $datosj=json_encode($vec);
+    echo $datosj;
+    header('Content-Type: application/json');
+
+
+?>
